f1d80ab0cfe2df20b628879df88a7864c7a60e87
riteshpandey7272 <riteshpandey7272@gmail.com>
2023-12-15 23:45:01 +0530

    navbar hide the skillmates

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -6,22 +6,35 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+
 const Header = () => {
+  
+  const [shouldHide, setShouldHide] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setShouldHide(window.innerWidth <= 647);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div>
-    <Navbar expand="lg" bg="dark" variant="dark">
-      <Container fluid>
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src=""
-            width="30"
-            height="30"
-            className="d-inline-block align-top ms-4"
-          />{' '}
-          Skill Mates
-        </Navbar.Brand>
+      <Navbar expand="lg" bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src=""
+              width="30"
+              height="30"
+              className="d-inline-block align-top ms-4"
+            />
+            {!shouldHide && <>Skill Mates</>}
+          </Navbar.Brand>
           <Form className="d-flex mx-auto col-lg-5">
             <Form.Control
               type="search"
@@ -31,26 +44,26 @@ const Header = () => {
             />
             <Button variant="outline-success">Search</Button>
           </Form>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
 
 
-          <Nav className="ms-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-            <NavDropdown title="SignIn" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Student</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Teacher</NavDropdown.Item>
-              <NavDropdown.Item href="#action5">School</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action6">
-                Choose Your Role..
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#action1">SignUp</Nav.Link>
-          </Nav>
+            <Nav className="ms-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+              <NavDropdown title="SignIn" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Student</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Teacher</NavDropdown.Item>
+                <NavDropdown.Item href="#action5">School</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action6">
+                  Choose Your Role..
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#action1">SignUp</Nav.Link>
+            </Nav>
 
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   )
 }
