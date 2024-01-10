7beccbe76a1dfcbd94bcbbda73f313b484934568
ritesh7272 <riteshpandey7272@gmail.com>
2024-01-10 23:48:19 +0530

    Modal style set

--- frontend/src/components/Modal/LoginModal.js
+++ frontend/src/components/Modal/LoginModal.js
@@ -5,30 +5,48 @@ import Modal from "react-modal";
 const LoginModal = ({ onClose }) => {
   const [modalIsOpen, setIsOpen] = useState(true);
 
-//   function openModal() {
-//     setIsOpen(true);
-//   }
-
-//   function afterOpenModal() {
-//     // references are now sync'd and can be accessed.
-//     subtitle.style.color = '#f00';
-//   }
-
   function closeModal() {
     setIsOpen(false);
   }
 
-
+  const customStyles = {
+    content: {
+      width: "30%",
+      height: "70%",
+      margin: "auto",
+      transition: "opacity 0.2s ease",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.8)",
+      transition: "opacity 0.2s ease",
+    },
+    closeButton: {
+      position: "absolute",
+      top: "5px",
+      right: "10px",
+      cursor: "pointer",
+      fontSize: "30px",
+      borderRadius: "50%",
+      transition: "background-color 0.3s ease",
+    },
+  };
 
   return (
-        <Modal
-            isOpen={modalIsOpen}
-           // onAfterOpen={afterOpenModal}
-            onRequestClose={closeModal}
-            contentLabel="Example Modal"
-        >
-        <SignIn visible={true}></SignIn>
-      </Modal>
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel="Example Modal"
+      style={customStyles}
+    >
+      <div style={customStyles.closeButton} onClick={closeModal}>
+        X
+      </div>
+      <SignIn visible={true}></SignIn>
+    </Modal>
   );
 };
 
