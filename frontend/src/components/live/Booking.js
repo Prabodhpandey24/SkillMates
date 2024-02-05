ea16b28d39036c0240b6fe9e6c41835d885f41b1
ritesh7272 <riteshpandey7272@gmail.com>
2024-02-05 23:22:32 +0530

    booking

--- frontend/src/components/live/Booking.js
+++ frontend/src/components/live/Booking.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import TimePicker from 'react-time-picker';
 
 const Booking = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [value, onChange] = useState('10:00');
   return (
     <div>
-      Booking
+      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+      <TimePicker onChange={onChange} value={value} />
     </div>
   )
 }
 
-export default Booking
+export default Booking;
